2c9734b9b0f7107cbcd158aa274e704339c987dd
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-11-21 23:03:08 -0800

    add findbyUser in refresh token repo
    Since unidirectional relationship, the refresh token searches for the passed in User inside one of the refresh tokens in refresh token repo

--- src/main/java/com/srikanth/security/demo/repository/RefreshTokenRepository.java
+++ src/main/java/com/srikanth/security/demo/repository/RefreshTokenRepository.java
@@ -3,10 +3,14 @@ package com.srikanth.security.demo.repository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.srikanth.security.demo.domain.RefreshToken;
+import com.srikanth.security.demo.domain.User;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Integer> {
     
     Optional<RefreshToken> findByUser_Username(String username);
     
     Optional<RefreshToken> findByRefreshToken(String refreshToken);
+    
+    Optional<RefreshToken> findByUser(User user);
+
 }
